f980ca37ab6dda4e926ca12ec182affb101312fe
lzy <lzy@liev.com>
2021-01-10 16:02:32 +0800

    deleted 5.mtcnn/03.mtcnn

--- 5.mtcnn/03.mtcnn/example/4.show_celeba.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from PIL import ImageDraw
-import os
-
-img = Image.open("")
-imgDraw = ImageDraw.Draw(img)
-# 70   8  68 119
-imgDraw.rectangle((70, 8, 70 + 68, 8 + 119), outline="red")
-img.show()
